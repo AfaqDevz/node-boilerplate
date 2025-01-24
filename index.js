d4de7428e617e7affeaf49362af62fa620426030
AfaqDevz <business@afaq.dev>
2025-01-25 01:25:56 +0500

    local

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: ["http://localhost:3001"],
+    origin: ["http://localhost:3000", "http://localhost:3001", "http://localhost:3002"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
