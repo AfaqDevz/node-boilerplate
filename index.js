7fe1d9da784786a8f4b7270b79b9cf1fecf88d8d
AfaqDevz <business@afaq.dev>
2025-01-25 03:21:55 +0500

    domains

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: ["http://localhost:3000", "http://localhost:3001", "http://localhost:3002"],
+    origin: ["http://localhost:3000", "http://localhost:3001", "http://localhost:3002", "https://smit-2025.vercel.app", "https://smit.afaq.dev"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
