4d147092c1698b9f68a2302171517445ea928d4a
AfaqDevz <business@afaq.dev>
2025-01-26 06:21:24 +0500

    Updated

--- middleware/authValid.js
+++ middleware/authValid.js
@@ -28,3 +28,23 @@
 // }
 
 // export default authValid;
+
+import jwt from 'jsonwebtoken';
+
+async function authValid(req, res, next) {
+    const token = req.header('Authorization');
+
+    if (!token) {
+        return res.status(401).json({ message: 'No token, authorization denied' });
+    }
+
+    try {
+        const decoded = jwt.verify(token.split(' ')[1], 'jwtSecret');
+        req.user = decoded.user;
+        next();
+    } catch (err) {
+        res.status(401).json({ message: 'Token is not valid' });
+    }
+};
+
+export default authValid;
--- routes/auth.js
+++ routes/auth.js
@@ -1,51 +1,51 @@
-import express from "express";
-import Joi from "joi";
-import 'dotenv/config'
-import sendRes from "../helper/sendRes.js";
-import User from "../models/user.js";
-import bcrypt from 'bcrypt'
-import jwt from 'jsonwebtoken'
+// import express from "express";
+// import Joi from "joi";
+// import 'dotenv/config'
+// import sendRes from "../helper/sendRes.js";
+// import User from "../models/user.js";
+// import bcrypt from 'bcrypt'
+// import jwt from 'jsonwebtoken'
 
-const router = express.Router();
+// const router = express.Router();
 
-const signupSchema = Joi.object({
-    username: Joi.string().min(3).max(30).required(),
-    email: Joi.string().email().required(),
-    password: Joi.string().min(8).required()
-})
+// const signupSchema = Joi.object({
+//     username: Joi.string().min(3).max(30).required(),
+//     email: Joi.string().email().required(),
+//     password: Joi.string().min(8).required()
+// })
 
-const loginSchema = Joi.object({
-    email: Joi.string().email().required(),
-    password: Joi.string().min(8).required()
-})
+// const loginSchema = Joi.object({
+//     email: Joi.string().email().required(),
+//     password: Joi.string().min(8).required()
+// })
 
-router.post('/signup', async (req, res) => {
-    const { error, value } = signupSchema.validate(req.body);
-    if (error) return sendRes(res, 400, null, true, error.message);
-    const user = await User.findOne({ email: value.email }).lean()
-    if (user) return sendRes(res, 403, null, true, 'User already exists.');
+// router.post('/signup', async (req, res) => {
+//     const { error, value } = signupSchema.validate(req.body);
+//     if (error) return sendRes(res, 400, null, true, error.message);
+//     const user = await User.findOne({ email: value.email }).lean()
+//     if (user) return sendRes(res, 403, null, true, 'User already exists.');
 
-    const hashedPassword = await bcrypt.hash(value.password, 10)
-    value.password = hashedPassword;
-    let newUser = new User({ ...value });
-    newUser = await newUser.save();
+//     const hashedPassword = await bcrypt.hash(value.password, 10)
+//     value.password = hashedPassword;
+//     let newUser = new User({ ...value });
+//     newUser = await newUser.save();
 
-    sendRes(res, 201, newUser, false, 'User created succesfully!');
-})
+//     sendRes(res, 201, newUser, false, 'User created succesfully!');
+// })
 
-router.post('/login', async (req, res) => {
-    const { error, value } = loginSchema.validate(req.body);
-    if (error) return sendRes(res, 400, null, true, error.message);
-    const user = await User.findOne({ email: value.email }).lean();
-    if (!user) return sendRes(res, 403, null, true, 'User not exists');
+// router.post('/login', async (req, res) => {
+//     const { error, value } = loginSchema.validate(req.body);
+//     if (error) return sendRes(res, 400, null, true, error.message);
+//     const user = await User.findOne({ email: value.email }).lean();
+//     if (!user) return sendRes(res, 403, null, true, 'User not exists');
 
-    const passwordChecker = await bcrypt.compare(value.password, user.password);
-    if (!passwordChecker) return sendRes(res, 403, null, true, 'Wrong Password or Email');
-    delete user.password
+//     const passwordChecker = await bcrypt.compare(value.password, user.password);
+//     if (!passwordChecker) return sendRes(res, 403, null, true, 'Wrong Password or Email');
+//     delete user.password
 
-    var token = jwt.sign({ ...user }, process.env.AUTH_SECRET);
+//     var token = jwt.sign({ ...user }, process.env.AUTH_SECRET);
 
-    sendRes(res, 200, { user, token }, false, 'User logged in!')
-})
+//     sendRes(res, 200, { user, token }, false, 'User logged in!')
+// })
 
-export default router
+// export default router
